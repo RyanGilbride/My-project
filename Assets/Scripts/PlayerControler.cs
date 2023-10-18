107e5681993eeca5a4a194dc91f4913fd2d214ac
RyanGilbride <147630020+RyanGilbride@users.noreply.github.com>
2023-10-18 11:12:15 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 14.0f;
+
+    public GameObject projectilePrefab;
+
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
+
+    void Start()
+    {
+        
+    }
+
+void Update()
+{
+if (transform.position.x < -xRange)
+{
+transform.position = new Vector3(-xRange, transform.position.y,
+transform.position.z);
+}
+if (transform.position.x > xRange)
+{
+transform.position = new Vector3(xRange, transform.position.y,
+transform.position.z);
+}
+if(transform.position.z < zMin)
+{
+transform.position = new Vector3(transform.position.x,
+transform.position.y, zMin);
+}
+if(transform.position.z > zMax)
+{
+transform.position = new Vector3(transform.position.x,
+transform.position.y, zMax);
+}
+horizontalInput = Input.GetAxis("Horizontal");
+transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+verticalInput = Input.GetAxis("Vertical");
+transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+if (Input.GetKeyDown(KeyCode.Space))
+{
+Instantiate(projectilePrefab, transform.position,
+projectilePrefab.transform.rotation);
+}
+}
+}
